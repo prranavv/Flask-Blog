255225bc7f15a15891a25fb6a1c4bd6a67f3ca80
Pranav Veerendra Kumar <kpranav637@gmail.com>
2022-12-26 18:40:39 +0530

    Just before hashing the password

--- website/__init__.py
+++ website/__init__.py
@@ -3,15 +3,33 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+db=SQLAlchemy()     
+DB_NAME="database.db"       #name of database is stored in this variable
+
+
+
 def create_app():
     app=Flask(__name__)
-    app.config['SECRET_KEY']="helloworld"
-
+    app.config['SECRET_KEY']="helloworld"                             #evry app has a secret key and in this case we defined it as helloworld
+    app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    
     from .views import views
     from .auth import auth
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
+    from .models import User  #Imports the user table from models.py
+    with app.app_context():
+        db.create_all()
+        print("Created Database")
+                                                    
+    login_manager=LoginManager()            #Login Manager created
+    login_manager.login_view="auth.login"  #if someone is not logged in we redirect them to auth.login
+    login_manager.init_app(app)
 
+    @login_manager.user_loader              #used to get information about username provided the id 
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
-                      
+
--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,51 @@
-from flask import Blueprint, render_template,redirect,url_for
+from flask import Blueprint, render_template,redirect,url_for, request,flash
+from .import db
+from .models import User
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth=Blueprint("auth",__name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET','POST'])
 def login():
+    email=request.form.get("email")
+    password=request.form.get("password")
     return render_template("login.html")
 
-@auth.route("/signup")
+@auth.route("/signup", methods=['GET','POST'])    #the get and post method is used to get or post links. ex when i submit my name in a form it has to be stored and posted on the server
 def sign_in():
+    if request.method == 'POST':
+        username=request.form.get("username")
+        email=request.form.get("email")
+        password1=request.form.get("password1")
+        password2=request.form.get("password2")
+
+        email_exists=User.query.filter_by(email=email).first()
+        username_exists=User.query.filter_by(username=username).first()
+        if email_exists:
+            flash('Email is already in use', category='error')
+        
+        elif username_exists:
+            flash('Username is already in use', category='error')
+        
+        elif password1!=password2:
+            flash('Password don\'t match',category='error')
+
+        elif len(username)<2:
+            flash('Username is too short',category='error')
+        
+        elif len(password1)<6:
+            flash('Password is too short',category='error')
+
+        elif len(email)<4:
+            flash('Email is invalid',category='error')
+
+        else:
+            new_user=User(email=email,username=username,password=password1)
+            db.session.add(new_user)    #add the new user to the staging area like in git
+            db.session.commit()         #adds the new user to the database
+            flash('User created!')
+            return redirect(url_for('views.home'))
     return render_template("signup.html")
 
 @auth.route("/logout")
